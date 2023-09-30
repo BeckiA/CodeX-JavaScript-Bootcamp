ed5b009243bec1cb8fbfa12954ea8067a85478a4
becki <berekeetabera@gmail.com>
2023-09-30 18:10:19 +0300

    Day-2 Working on Equality Operator

---
+++ Day-2/equality_operators.js
@@ -0,0 +1,11 @@
+
+// Strict Equality(===) with Weak Equality(==)
+
+// 1. Strict Equality
+// - Strict equality will compare based without of any type conversion 
+console.log(1 === 1);
+console.log(1 === true);
+
+// 2. Weak Equality
+// -Weak equality will use type conversions based on a certain property
+console.log(1 == true);
