373aeba7596d3bd019a38a6404242bbfbd2c471c
becki <berekeetabera@gmail.com>
2023-09-30 17:43:50 +0300

    Day-2 Practicing on Arthimetic Operators

---
+++ Day-2/arthimetic_operators.js
@@ -0,0 +1,20 @@
+
+// Basic Arthimetic Operators in JavaScript
+let x = 10;
+let y = 4;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+// Increment and Decrement Operators
+// 1. Incrementation
+console.log(++y)
+console.log(x++); // Will Print out 10 but if we log x it is incremented to 11
+console.log(x);
+
+// 2. Decrementation
+console.log(--x);
+console.log(y--); // Will Print out 4 but if we log y it is decremented to 3
+console.log(y);
