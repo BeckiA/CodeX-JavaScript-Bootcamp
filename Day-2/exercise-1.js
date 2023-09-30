326bf817fe5eefe81c0b0d0e6aab74fbdb5e732c
becki <berekeetabera@gmail.com>
2023-09-30 20:31:13 +0300

    Day-2 Excersice On Swapping Values

---
+++ Day-2/exercise-1.js
@@ -0,0 +1,14 @@
+
+let a = 'red';
+let b = 'blue';
+console.log(a);
+console.log(b);
+
+// Exersice Swap the value of a and b
+// Such that a: blue, b: red
+// Method -1
+let c = a;
+a = b;
+b = c;
+console.log('a: ', a);
+console.log('b: ', b);
