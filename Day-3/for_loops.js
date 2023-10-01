fdadedd8d4d4de4cf9bc2b43fbab1c7f18cd1edc
becki <berekeetabera@gmail.com>
2023-10-01 21:34:11 +0300

    Day-3 Exersicing on For Loops

---
+++ Day-3/for_loops.js
@@ -0,0 +1,20 @@
+// Working On For Loops
+// 1. Displaying Hello World Five Times
+for (let i = 1; i <= 5; i++) {
+    console.log("Hello World", i);
+    
+}
+
+// 2. Displaying Odd Numbers between 1 and 5:
+for (let i = 1; i <= 5; i++){
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+// 3. Displaying Odd Numbers in Reverse Order:
+for( let i = 5; i >= 1; i-- ){
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
