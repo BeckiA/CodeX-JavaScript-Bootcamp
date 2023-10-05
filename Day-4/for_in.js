dd00de06ae40b377550ea350db893bd30f249b91
becki <berekeetabera@gmail.com>
2023-10-05 10:56:00 +0300

    Day-4 Working on for of loop

--- Day-4/for_in.js
+++ Day-4/for_in.js
@@ -8,3 +8,10 @@ const person = {
 for (const key in person) {
     console.log(key,person[key]);
 }
+
+let colors = ['red', 'green', 'blue'];
+
+for (let index in colors){
+    console.log(index, colors[index]);
+}
+
---
+++ Day-4/for_of.js
@@ -0,0 +1,10 @@
+
+// 2. For of Looping
+let colors = ['red', 'green', 'blue'];
+
+// for ... of Loop
+// => We can use the for...of loop to iterate over an array without having idea
+// about the index of the array!
+for (const color of colors) {
+    console.log(color)
+}
