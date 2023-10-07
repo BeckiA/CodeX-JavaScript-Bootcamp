4b36e3b26ebbc3a50878bf8fdc8725d876b3be8e
becki <berekeetabera@gmail.com>
2023-10-08 00:10:07 +0300

    Day-6 Exersicing on the sum of 3 and 5

---
+++ Day-6/Exersices/working_on_multiples_of_3&5.js
@@ -0,0 +1,30 @@
+
+// Given the limits of Numbers find the sum of multiples
+// Of both 3 and 5
+
+// Example: Multiples of 3: 3,6,9
+// Multiples of 5: 5,10,15
+
+console.log(sum(10));
+function sum(limit){
+    let sum = 0;
+
+        // MOSH'S WAY
+    for (let i = 0; i <= limit; i++)
+        if (i % 3 === 0 || i % 5 === 0) 
+            sum += i;
+    
+
+    return sum;
+        // MY WAY!
+
+        // if (i % 3 == 0) {
+        //     sum += i;
+        // }else if(i % 5 == 0){
+        //     sum += i;
+        // } 
+    // return sum;       
+        
+    
+
+}
